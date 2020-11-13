e719c59871532fefb0b5a40f15bef2cfc9357573
MaxJa4 <74194322+MaxJa4@users.noreply.github.com>
2020-11-13 16:42:30 +0100

    Add files via upload
    First algorithm from sheet 01

---
+++ Blatt01/server.py
@@ -0,0 +1,44 @@
+from mesa.visualization.modules import CanvasGrid
+from mesa.visualization.ModularVisualization import ModularServer
+from mesa.visualization.modules import ChartModule
+from model import AntModel
+from agents import AntAgent, ParticleAgent
+
+
+def agent_portrayal(agent):
+    portrayal = {}
+
+    if type(agent) is AntAgent:
+        portrayal = {"Shape": "circle",
+                     "Color": "red",
+                     "Filled": "true",
+                     "Layer": 0,
+                     "r": 0.5}
+    elif type(agent) is ParticleAgent:
+        portrayal = {"Shape": "circle",
+                     "Color": "gray",
+                     "Filled": "true",
+                     "Layer": 0,
+                     "r": 0.3}
+
+    return portrayal
+
+
+grid = CanvasGrid(agent_portrayal, 50, 50, 700, 700)
+
+chart1 = ChartModule([{"Label": "Particles with neighbors",
+                      "Color": "Black"}],
+                    data_collector_name='datacollector')
+
+chart2 = ChartModule([{"Label": "Particles with neighbors percentage",
+                      "Color": "Black"}],
+                    data_collector_name='datacollector')
+
+server = ModularServer(AntModel,
+                       [grid, chart1, chart2],
+                       "Ant Model",
+                       {"N": 1, "particle_density": 0.2,
+                        "step_size": 1, "jump_size": 3, "all_in_center": False})
+
+server.port = 8521  # The default
+server.launch()
